7fc1919f1d393e183b27e8d08a95c4e3e71c40ef
stephaniehohenberg <stephanie.hohenberg@gmail.com>
2025-04-11 19:25:13 -0600

    updates deployment

--- src/app/services/tracker.service.ts
+++ src/app/services/tracker.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {UtmParameters} from '../data/utm.data';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {environment} from '../../environment/environment';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
