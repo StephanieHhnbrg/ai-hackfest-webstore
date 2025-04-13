5d846cc98053530b9365fc85801a8a14439a9235
stephaniehohenberg <stephanie.hohenberg@gmail.com>
2025-04-12 18:27:04 -0600

    adds deployed page to allowed origins in GCloud Run scripts to avoid CORS exceptions

--- gcloudrun/track_webpage_call.py
+++ gcloudrun/track_webpage_call.py
@@ -18,17 +18,27 @@ def update_tracking_db():
     "timestamp": firestore.SERVER_TIMESTAMP
   })
 
+
+ALLOWED_ORIGINS = [
+  'http://localhost:4200',
+  'https://stephaniehhnbrg.github.io'
+]
+
+
 def handle_cors():
   response = make_response()
   response.status_code = 204
-  response.headers['Access-Control-Allow-Origin'] = 'http://localhost:4200'
+  for origin in ALLOWED_ORIGINS:
+    response.headers['Access-Control-Allow-Origin'] = origin
   response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
   response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
   return response
 
 
-def create_response():
-  response = make_response()
+def create_response(data):
+  response = make_response(jsonify(data))
   response.status_code = 200
-  response.headers['Access-Control-Allow-Origin'] = 'http://localhost:4200'
+  for origin in ALLOWED_ORIGINS:
+    response.headers['Access-Control-Allow-Origin'] = origin
   return response
+
--- gcloudrun/track_webpage_purchase.py
+++ gcloudrun/track_webpage_purchase.py
@@ -47,17 +47,27 @@ def get_time_from_req(request_json, request_args):
   else:
     return 0
 
+
+ALLOWED_ORIGINS = [
+  'http://localhost:4200',
+  'https://stephaniehhnbrg.github.io'
+]
+
+
 def handle_cors():
   response = make_response()
   response.status_code = 204
-  response.headers['Access-Control-Allow-Origin'] = 'http://localhost:4200'
+  for origin in ALLOWED_ORIGINS:
+    response.headers['Access-Control-Allow-Origin'] = origin
   response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
   response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
   return response
 
 
-def create_response():
-  response = make_response()
+def create_response(data):
+  response = make_response(jsonify(data))
   response.status_code = 200
-  response.headers['Access-Control-Allow-Origin'] = 'http://localhost:4200'
+  for origin in ALLOWED_ORIGINS:
+    response.headers['Access-Control-Allow-Origin'] = origin
   return response
+
